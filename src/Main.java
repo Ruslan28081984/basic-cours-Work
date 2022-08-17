4a5fc55b8705c11980a22ce50414732e4d54502b
user1 <89201828020@mail.ru>
2022-08-17 19:56:29 +0300

    второй коммит- начал делать в ветке мастер, случайно

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee[] employees = new Employee[10];
+        for (int i = 0; i < employees.length; i++) {
+
+
+        }
+
     }
 }
